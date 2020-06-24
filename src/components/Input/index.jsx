5bcc04db60ef5569922e8d82a419de9eefd05eb6
MaDRaGe <locodi111@mail.ru>
2020-06-24 18:23:59 +0300

    Input.jsx added

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Input extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <input />;
+  }
+}
+
+export default Input;
